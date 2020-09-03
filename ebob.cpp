bdedcd9722613464dcbf4dfdf05a3a60a6f66a20
elif <48827713+elifunlu58@users.noreply.github.com>
2020-09-03 20:13:04 +0300

    Add files via upload

---
+++ ebob.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int en_buyuk(int sayi1,int sayi2)
+
+{
+	if (sayi1>sayi2)
+	return sayi1;
+	else
+	return sayi2;
+}
+
+
+int main()
+ {
+ 	bas:
+ 		int sayi1.sayi2;
+ 		cout<<"sayi1=";
+ 		cin>>sayi1;
+ 		cout<<'\n';
+	
+	return 0;
+	
+	
+	
+	
+	
+	return 0;
+}
